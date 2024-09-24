aa4cb7c3ce460c4d89ee03c276554eb2a692a25d
Dorcas <dorcasjemosop@gmail.com>
2024-09-24 16:04:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myjournal/database/NoteDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.myjournal.database
+
+import android.content.Context
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.myjournal.models.Note
+import com.example.myjournal.utilities.DATABASE_NAME
+
+
+@Database(entities = arrayOf(Note::class) , version = 1, exportSchema = false)
+ abstract class NoteDatabase : RoomDatabase() {
+
+     abstract fun getNoteDao(): Dao
+
+     companion object {
+         @Volatile
+         private var INSTANCE : NoteDatabase?= null
+
+         fun getDatabase(context: Context): NoteDatabase{
+             return INSTANCE?: synchronized(this) {
+                 val instance = Room.databaseBuilder(
+                     context.applicationContext,
+                     NoteDatabase::class.java,
+                     DATABASE_NAME
+                 ).build()
+                 INSTANCE= instance
+                 instance
+             }
+         }
+     }
+
+}
---
+++ app/src/main/java/com/example/myjournal/utilities/constants.kt
@@ -0,0 +1,3 @@
+package com.example.myjournal.utilities
+
+const val DATABASE_NAME = "note_database"
